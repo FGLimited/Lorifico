169ccc2f35786e5edc10d30481c96d76c5e1bc2a
Andrea Guglielmini <il.guglio95@gmail.com>
2017-06-06 11:27:34 +0200

    Communication Manager supporting UserSpecific Actions

--- src/Client/Datawarehouse.java
+++ src/Client/Datawarehouse.java
@@ -8,16 +8,18 @@ import Game.UserObjects.GameUser;
 import Logging.Logger;
 import Model.User.User;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Datawarehouse keeps data received from server.
  */
 public class Datawarehouse {
     private static Datawarehouse datawarehouse;//singleton
 
-    private User user;
-    private GameUser gameUser;
-
-    //.......
+    private User myUser;
+    private String myUsername;
+    private Map<String, GameUser> gameUserMap = new HashMap<>();
 
     private Datawarehouse() {
     }
@@ -32,22 +34,64 @@ public class Datawarehouse {
         return datawarehouse;
     }
 
-    public User getUser() {
-        return user;
+    public User getMyUser() {
+        return myUser;
     }
 
-    public void setUser(User user) {
-        //observer pattern?
-        this.user = user;
-        Logger.log(Logger.LogLevel.Normal, "User received: [" + user.getUsername() + ", ]");
+    /**
+     * Sets current myUser remote User object
+     *
+     * @param user
+     */
+    public void setMyUser(User user) {
+        this.myUser = user;
+        this.myUsername = user.getUsername();
+        Logger.log(Logger.LogLevel.Normal, "MyUser received: [" + user.getUsername() + ", ]");
     }
 
-    public GameUser getGameUser() {
-        return gameUser;
+    /**
+     * Returns true if this is current myUser's username
+     *
+     * @param username
+     * @return
+     */
+    public boolean isMe(String username) {
+        return username.equals(myUsername);
     }
 
-    public void setGameUser(GameUser gameUser) {
-        //observer pattern?
-        this.gameUser = gameUser;
+    /**
+     * Retrieves my username
+     *
+     * @return
+     */
+    public String getMyUsername() {
+        return myUsername;
+    }
+
+    /**
+     * Retrieves myGameUser
+     *
+     * @return
+     */
+    public GameUser getMyGameUser() {
+        return this.getGameUser(getMyUsername());
+    }
+
+    /**
+     * Retrieves gameUser's of specified myUser
+     *
+     * @param username
+     * @return
+     */
+    public GameUser getGameUser(String username) {
+        return gameUserMap.get(username);
+    }
+
+    public void setGameUser(String username, GameUser gameUser) {
+        if (gameUserMap.containsKey(username)) {
+            gameUserMap.replace(username, gameUser);
+        } else {
+            gameUserMap.put(username, gameUser);
+        }
     }
 }
