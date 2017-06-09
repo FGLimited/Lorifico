b296ac54aba5899123e2bdd36b7b4bb1ff4c4b6a
andrea <il.guglio95@gmail.com>
2017-06-09 12:54:55 +0200

    Fixed broken assignation in PlayersBoxController

--- src/Client/UI/GUI/resources/gameComponents/PlayersBoxController.java
+++ src/Client/UI/GUI/resources/gameComponents/PlayersBoxController.java
@@ -3,6 +3,7 @@ package Client.UI.GUI.resources.gameComponents;
 import Client.Datawarehouse;
 import Client.UI.PlayerStateObserver;
 import Game.UserObjects.PlayerState;
+import Logging.Logger;
 import javafx.application.Platform;
 import javafx.scene.layout.HBox;
 
@@ -13,7 +14,7 @@ public class PlayersBoxController implements PlayerStateObserver {
     private HBox node;//Node where we are attaching our UserAvatarClickableElements
 
     public PlayersBoxController(HBox playersHBox, MiliyaryVictoryBoxController miliyaryVictoryBoxController) {
-        this.node = node;
+        this.node = playersHBox;
         Datawarehouse.getInstance().registerPlayerStateObserver(this);
     }
 
@@ -31,12 +32,14 @@ public class PlayersBoxController implements PlayerStateObserver {
             return;
         }
 
+        Logger.log(Logger.LogLevel.Normal, "PlayersBoxController just received a PlayerState");
+
         //if we have a link to node, check if user's are shown on GUI
         if (node != null) {
             //If user related to this PlayerState is not shown on GUI, let's attach it.
             if (!node.getChildren().contains(UserAvatarClickableElement.getInstance(username))) {
                 node.getChildren().add(UserAvatarClickableElement.getInstance(username));
             }
-        }
+        } else Logger.log(Logger.LogLevel.Error, "PlayersBoxController, node is null");
     }
 }
