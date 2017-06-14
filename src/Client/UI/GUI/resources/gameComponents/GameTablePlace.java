4a2bd47a3d818eafca3d6977331b0f1a829e700f
andrea <il.guglio95@gmail.com>
2017-06-14 12:59:17 +0200

    WIP: Linking GameTablePlaces to game

--- src/Client/UI/GUI/resources/gameComponents/GameTablePlace.java
+++ src/Client/UI/GUI/resources/gameComponents/GameTablePlace.java
@@ -1,5 +1,9 @@
 package Client.UI.GUI.resources.gameComponents;
 
+import Action.BaseAction;
+import Action.Move;
+import Client.CommunicationManager;
+import Server.Game.UserObjects.Domestic;
 import javafx.scene.effect.ColorAdjust;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -8,6 +12,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.transform.Translate;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by andrea on 31/05/17.
  */
@@ -15,7 +23,18 @@ public class GameTablePlace extends StackPane {
     private final String COVERINGS_BASE_URL = "Client/UI/GUI/resources/images/gameTable";
     private final int Z_POS = -1;
     private Translate translate;
+    private int id;//id of this covering
+    private int capacity;//Max capacity of this position
+    private Map<Domestic, Domestic3D> occupantsDomesticMap = new HashMap<>();
 
+    private GameTablePlace binding = null;//GameTablePlace we are binded to, if that
+    // binding place is free then no domestic can be placed in this
+
+
+    protected GameTablePlace(int id, int capacity, boolean isCovered, GameTablePlace binding, double width, double height, double xCoveringPos, double yCoveringPos, double xRegionPos, double yRegionPos, double radiusX, double radiusY) {
+        this(id, capacity, isCovered, width, height, xCoveringPos, yCoveringPos, xRegionPos, yRegionPos, radiusX, radiusY);
+        this.binding = binding;
+    }
     /**
      * Creates a gameTablePlace that could be covered or be click-able.
      *
@@ -31,6 +50,8 @@ public class GameTablePlace extends StackPane {
      * @param radiusY
      */
     protected GameTablePlace(int id, int capacity, boolean isCovered, double width, double height, double xCoveringPos, double yCoveringPos, double xRegionPos, double yRegionPos, double radiusX, double radiusY) {
+        this.capacity = capacity;
+        this.id = id;
         if (isCovered) {
             final String url = COVERINGS_BASE_URL + "/covering" + id + ".png";
             createCoveringStackPane(url, width, height, xCoveringPos, yCoveringPos, Z_POS);
@@ -50,6 +71,8 @@ public class GameTablePlace extends StackPane {
      */
     protected GameTablePlace(int id, int capacity, double xRegionPos, double yRegionPos, double radiusX, double radiusY) {
         createClickableRegion(xRegionPos, yRegionPos, Z_POS, radiusX, radiusY);
+        this.capacity = capacity;
+        this.id = id;
     }
 
 
@@ -95,10 +118,14 @@ public class GameTablePlace extends StackPane {
         translate = new Translate(xPos, yPos, zPos);
         getTransforms().add(translate);
 
+
         //We highlight it when mouse is over it
-
-
         ellipse.setOnMouseEntered(event -> {
+            //Check if this position is bound to another one
+            if (binding != null) {
+                //If position we are bound to is not full, abort
+                if (!binding.isFull()) return;
+            }
             ColorAdjust colorAdjust = new ColorAdjust();
             colorAdjust.setContrast(0.1);
             colorAdjust.setHue(-0.05);
@@ -112,10 +139,26 @@ public class GameTablePlace extends StackPane {
             ellipse.setEffect(null);
             ellipse.setOpacity(0);
         });
+
+        //Callback on click
+        ellipse.setOnMouseClicked(event -> {
+            //Check if this position is bound to another one
+            if (binding != null) {
+                //If position we are bound to is not full, abort
+                if (!binding.isFull()) return;
+            }
+
+            //Else send message to server asking to occupy this position
+            int position = (this.id + occupantsDomesticMap.size());
+            BaseAction action = new Move(position, Collections.emptyList());
+            CommunicationManager.getInstance().sendMessage(action);
+            System.out.println("Asking server to occupy " + position);
+        });
+
     }
 
-    //Debugging / placing purpose
-    public Translate getTranslate() {
-        return translate;
+    public boolean isFull() {
+        if (occupantsDomesticMap.size() >= capacity) return true;
+        return false;
     }
 }
--- src/Client/UI/GUI/resources/gameComponents/GameTablePlacesBlock.java
+++ src/Client/UI/GUI/resources/gameComponents/GameTablePlacesBlock.java
@@ -1,5 +1,6 @@
 package Client.UI.GUI.resources.gameComponents;
 
+import Client.Datawarehouse;
 import javafx.scene.Group;
 
 /**
@@ -9,15 +10,22 @@ public class GameTablePlacesBlock extends Group {
 
     public GameTablePlacesBlock() {
         //Place Coverings / Clickable Positions
+        int playingUsers = Datawarehouse.getInstance().getMatchAttendees().size();
+
         getChildren().add(new GameTablePlace(50, 4, 430, 78.5, 135, 57));
-        getChildren().add(new GameTablePlace(42, 1, false, 90, 105, 680.5, 455.5, 684, 462, 43, 40));
-        getChildren().add(new GameTablePlace(43, 1, false, 91.17, 108.2, 748.5, 520, 751.5, 531.5, 43, 40));
+        getChildren().add(new GameTablePlace(42, 1, (playingUsers < 4 ? true : false), 90, 105, 680.5, 455.5, 684, 462, 43, 40));
+        getChildren().add(new GameTablePlace(43, 1, (playingUsers < 4 ? true : false), 91.17, 108.2, 748.5, 520, 751.5, 531.5, 43, 40));
         getChildren().add(new GameTablePlace(41, 1, 595.5, 437, 43, 40));
         getChildren().add(new GameTablePlace(40, 1, 501.5, 436.5, 43, 40));
-        getChildren().add(new GameTablePlace(30, 1, 7.5, 461.5, 43, 40));
-        getChildren().add(new GameTablePlace(20, 1, 8, 584, 43, 40));
-        getChildren().add(new GameTablePlace(21, 3, false, 226, 111.8, 116.5, 575.5, 130.5, 587, 100, 35));
-        getChildren().add(new GameTablePlace(31, 3, false, 224, 105.7, 116, 457, 130, 465, 100, 35));
+
+
+        GameTablePlace harvestPlace = new GameTablePlace(20, 1, 8, 584, 43, 40);
+        getChildren().add(new GameTablePlace(21, 3, (playingUsers < 3 ? true : false), harvestPlace, 226, 111.8, 116.5, 575.5, 130.5, 587, 100, 35));
+        getChildren().add(harvestPlace);
+
+        GameTablePlace productionPlace = new GameTablePlace(30, 1, 7.5, 461.5, 43, 40);
+        getChildren().add(new GameTablePlace(31, 3, (playingUsers < 3 ? true : false), productionPlace, 224, 105.7, 116, 457, 130, 465, 100, 35));
+        getChildren().add(productionPlace);
     }
 
     /**
